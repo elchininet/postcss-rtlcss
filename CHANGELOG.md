aa65275e132b1d1068e0518831dd603aded6ff9e
Iván Pereira Muñoz <elchininet@gmail.com>
2020-04-12 17:13:59 +0200

    Multiple changes:
    - Improve the output formatting
    - Refactored multiple utility methods to make them simpler
    - Created a CHANGELOG

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# Changelog
+
+## [1.0.2] - 2020-04-12
+
+- Improve the output formatting
+- Refactored multiple utility methods to make them simpler
+- Created a CHANGELOG
+
+## [1.0.1] - 2020-04-11
+
+- Refactored the exported module to be a named exported instead of a default one
+
+## [1.0.0] - 2020-04-11
+
+- Created the postcss-rtlcss package
--- src/utilities/comments.ts
+++ src/utilities/comments.ts
@@ -1,12 +1,9 @@
-import { Comment, Rule, Node } from 'postcss';
+import { Comment } from 'postcss';
 import {
-    COMMENT_TYPE,
-    DECLARATION_TYPE,
-    RTL_COMMENT_REGEXP,
     RTL_COMMENT_IGNORE_REGEXP,
     IGNORE_NEXT,
     IGNORE_BEGIN,
-    IGNORE_END,
+    IGNORE_END
 } from '@constants';
 
 export const getIgnoreComment = (comment: Comment): string | null => {
@@ -20,22 +17,4 @@ export const getIgnoreComment = (comment: Comment): string | null => {
         }
     }
     return null;
-};
-
-export const removeRTLComments = (rule: Rule): void => {
-    rule.walk((node: Node): void => {
-        if (node.type === COMMENT_TYPE) {
-            if (RTL_COMMENT_REGEXP.test(node.toString())) {
-                node.remove();
-            }
-        }
-        if (node.type === DECLARATION_TYPE) {
-            // @ts-ignore
-            if (node.raws && node.raws.value && RTL_COMMENT_REGEXP.test(node.raws.value.raw)) {
-                // @ts-ignore
-                delete node.raws.value;
-                node.value = node.value.trim();
-            }
-        }
-    });
 };
