5d7c637724e197a382131127b557747862969fc4
Iván Pereira Muñoz <elchininet@gmail.com>
2020-04-13 00:59:31 +0200

    Build an utility to loop through the children of a container taking into account the rtl comments

---
+++ src/utilities/containers.ts
@@ -0,0 +1,59 @@
+import { Container, Node, Comment } from 'postcss';
+import { COMMENT_TYPE, RULE_TYPE, IGNORE_MODE, IGNORE_BEGIN, IGNORE_END } from '@constants';
+import { getIgnoreComment } from '@utilities/comments';
+import { cleanRuleRawsBefore } from '@utilities/rules';
+
+type WalkContainerCallback = (node: Node) => void;
+
+export const walkContainer = (container: Container, filter: string, callback: WalkContainerCallback): void => {
+
+    let ignoreMode = IGNORE_MODE.DISABLED;
+
+    container.each((node: Node): undefined | false => {
+
+        if ( node.type !== COMMENT_TYPE && node.type !== filter ) return;
+
+        if (node.type === COMMENT_TYPE) {
+
+            const comment = node as Comment;
+            const ignore = getIgnoreComment(comment);
+            
+            if (ignore) {
+                if (filter === RULE_TYPE) {
+                    cleanRuleRawsBefore(comment.next());
+                }                
+                comment.remove();             
+                switch (ignore) {
+                    case IGNORE_BEGIN:
+                        ignoreMode = IGNORE_MODE.BLOCK_MODE;
+                        break;
+                    case IGNORE_END:
+                        ignoreMode = IGNORE_MODE.DISABLED;
+                        break;
+                    default:
+                        ignoreMode = IGNORE_MODE.NEXT_NODE;
+                        break;
+                }
+                return;
+            }
+
+        }
+
+        if (node.type === filter) {
+
+            if (ignoreMode === IGNORE_MODE.NEXT_NODE) {                
+                ignoreMode = IGNORE_MODE.DISABLED;
+                return;
+            }
+
+            if (ignoreMode === IGNORE_MODE.BLOCK_MODE) {
+                return;
+            }
+
+            callback(node);
+
+        }
+
+    });
+
+};
