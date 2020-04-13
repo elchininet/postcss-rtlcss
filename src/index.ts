56b4a5e45efe3823f257fef920ab3da13c196e93
Iván Pereira Muñoz <elchininet@gmail.com>
2020-04-13 13:39:27 +0200

    Refactor the rules and declarations parsers

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import postcss, { Root, Transformer } from 'postcss';
 import { PluginOptions, RulesObject } from '@types';
 import { parseOptions } from '@utilities/options';
-import { parseRules } from '@rules/parser';
+import { parseRules } from '@parsers/rules';
 import { appendRules } from '@utilities/rules';
 export { PluginOptions, Mode, Source, PluginStringMap,  } from '@types';
 
 const transformer = (options: PluginOptions = {}): Transformer  => (
     (css: Root): void => {
         const optionsParsed = parseOptions(options);
-        const result: RulesObject[] = parseRules(css, optionsParsed);
+        const result: RulesObject[] = [];
+        parseRules(result, css, optionsParsed);
         appendRules(result);           
     }
 );
