69aaf36eaa95e237b04b4e500cd86683773cc070
Iván Pereira Muñoz <elchininet@gmail.com>
2020-04-18 19:01:59 +0200

    Modify the header of the playground

--- playground/src/components/Header/Header.tsx
+++ playground/src/components/Header/Header.tsx
@@ -4,7 +4,9 @@ import { stylesheet } from './stylesheet';
 export const Header = (): JSX.Element => {
     return (
         <header css={stylesheet.wrapper}>
+            <span>{ '{' }&nbsp;</span>
             <h1 css={stylesheet.title}>PostCSS-RTLCSS online</h1>
+            <span>&nbsp;{ '}' }</span>
         </header>
     );
 };
--- playground/src/components/Header/stylesheet.tsx
+++ playground/src/components/Header/stylesheet.tsx
@@ -5,16 +5,20 @@ export const stylesheet = parse({
     wrapper: {
         alignItems: 'center',
         backgroundColor: COLORS.gray_darkest,
-        display: 'flex',
         flexGrow: 0,
         flexShrink: 0,
         height: 50,
         lineHeight: '50px',
-        padding: shorthands(0, 10, 0, 50)
+        padding: shorthands(0, 10, 0, 50),
+        '& span': {
+            color: COLORS.action,
+            fontSize: [18, 20],
+            fontWeight: 400
+        }
     },
     title: {
         color: COLORS.gray_light,
-        flexGrow: 1,
+        display: 'inline',
         fontSize: [14, 16],
         fontWeight: 700,
         margin: 0
