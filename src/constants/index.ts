05f0dc0abab02fae02f6a97f1da214c0cced7997
Iván Pereira Muñoz <elchininet@gmail.com>
2020-04-28 23:11:12 +0200

    Improve control directive regular expression

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -8,7 +8,7 @@ export const KEYFRAMES_NAME = 'keyframes';
 export const ANIMATION_PROP = 'animation';
 export const ANIMATION_NAME_PROP = 'animation-name';
 export const RTL_COMMENT_REGEXP = /rtl:/;
-export const RTL_CONTROL_DIRECTIVE_REG_EXP = /^\/\*!? *rtl:?(\w+)?:(\w+):?([^*]*?) *\*\/$/;
+export const RTL_CONTROL_DIRECTIVE_REG_EXP = /^\/\*!? *rtl:?(begin|end)?:(\w+):?([^*]*?) *\*\/$/;
 export const FLIP_PROPERTY_REGEXP = /(right|left)/i;
 
 export enum CONTROL_DIRECTIVE {
