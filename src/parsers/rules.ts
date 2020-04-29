abcf4cdbd6ac4a03d9032b324557a9e7bab3d4fe
Iván Pereira Muñoz <elchininet@gmail.com>
2020-04-30 00:32:44 +0200

    This commit implements the RTL raw control directive
    This directive parses the `CSS` parameter and inserts it in its place. Depending on the `source` parameter the parsed `CSS` will be treated as `rtl` or `ltr`

--- src/parsers/rules.ts
+++ src/parsers/rules.ts
@@ -1,20 +1,31 @@
-import { Container, Node, Rule, Comment } from 'postcss';
-import { ObjectWithProps, ControlDirective } from '@types';
+import postcss, { Container, Node, Rule, Comment } from 'postcss';
+import { ObjectWithProps, ControlDirective, Source } from '@types';
 import { RULE_TYPE, CONTROL_DIRECTIVE } from '@constants';
 import { store } from '@data/store';
 import { isIgnoreDirectiveInsideAnIgnoreBlock, checkDirective } from '@utilities/directives';
 import { walkContainer } from '@utilities/containers';
 import { cleanRuleRawsBefore } from '@utilities/rules';
+import { addSelectorPrefixes } from '@utilities/selectors';
 import { parseDeclarations } from './declarations';
 
 export const parseRules = (container: Container): void => {
 
     const controlDirectives: ObjectWithProps<ControlDirective> = {};
+    const { source, ltrPrefix, rtlPrefix } = store.options;
 
     walkContainer(
         container,
         [ RULE_TYPE ],
         (comment: Comment, controlDirective: ControlDirective): void => {
+
+            if (controlDirective.directive === CONTROL_DIRECTIVE.RAW && controlDirective.raw) {
+                const root = postcss.parse(controlDirective.raw);
+                root.walkRules((rule: Rule): void => {
+                    addSelectorPrefixes(rule, source === Source.ltr ? rtlPrefix : ltrPrefix);
+                });
+                comment.replaceWith(root.nodes);
+                return;
+            }
 
             cleanRuleRawsBefore(comment.next());              
             comment.remove(); 
