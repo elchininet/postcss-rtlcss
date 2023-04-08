efdedccc9da056963370339e919611deec5c1ec3
Iván Pereira Muñoz <elchininet@gmail.com>
2023-04-08 19:42:22 +0200

    Use mikepenz/release-changelog-builder-action@v3 to create the release body

--- scripts/extract_last_release_body.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs/promises');
-
-const createRelease = async() => {
-    const data = await fs.readFile('./CHANGELOG.md', { encoding: 'utf8' });
-    const RELEASE = data.replace(/^#[^#]*?##\s.*?(\d{4}-\d{2}-\d{2}[^#]*)\n[\s\S]*$/, '$1');
-    await fs.writeFile('./RELEASE.md', RELEASE);
-};
-
-createRelease();
