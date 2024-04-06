862f9519644523a8fa173becd55cff4a7c164fff
Iván Pereira Muñoz <elchininet@gmail.com>
2024-04-06 22:38:22 +0200

    Make aliases affect safe-prefixed rules

--- tests/constants/index.ts
+++ tests/constants/index.ts
@@ -1,5 +1,8 @@
 const PADDING = 'padding';
 const MARGIN = 'margin';
+const ANIMATION = 'animation';
+const BORDER_WIDTH = 'border-width';
+const BACKGROUND_CLIP = 'background-clip';
 
 export const aliases = {
   '--small-padding': PADDING,
@@ -7,4 +10,11 @@ export const aliases = {
   '--custom-margin': MARGIN,
   '--small-margin': MARGIN,
   '--large-margin': MARGIN
+};
+
+export const propsAliases = {
+  'view-timeline': ANIMATION,
+  'custom-border-width': BORDER_WIDTH,
+  'custom-clip-background': BACKGROUND_CLIP,
+  'custom-padding': PADDING
 };
