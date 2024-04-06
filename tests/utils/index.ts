9536b967f82f5af919dcd71f5bb36837fd297cb3
Iván Pereira Muñoz <elchininet@gmail.com>
2024-04-06 16:35:19 +0200

    Separate snapshots tests in different files

--- tests/utils/index.ts
+++ tests/utils/index.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import { PluginOptions, Mode } from '../../src/@types';
 
-export const readCSSFile = (name: string): Promise<string> => new Promise((resolve, reject) => {
+export const readCSSFile = (name: string): Promise<string> => new Promise((resolve) => {
     fs.readFile(`${__dirname}/../css/${name}`, 'utf8', (error, data): void => {
         if (error) {
             resolve('');
@@ -15,4 +15,13 @@ export const runTests = (options: PluginOptions, callback: (options: PluginOptio
     callback({...options, mode: Mode.combined});
     callback({...options, mode: Mode.override});
     callback({...options, mode: Mode.diff});
+};
+
+export const createSnapshotFileName = (baseFileName: string, testName: string, mode: string | undefined): string => {
+    const folder = './__snapshots__';
+    const extension = 'snapshot';
+    if (mode) {
+        return `${folder}/${baseFileName}/${mode}/${testName}.${extension}`;
+    }
+    return `${folder}/${baseFileName}/${testName}.${extension}`;
 };
