8602e788729b1c6417b82c1c99b0b7b1ee82565f
Iván Pereira Muñoz <elchininet@gmail.com>
2024-04-11 22:38:43 +0200

    Split aliases variables in different snapshots

--- tests/aliases-variables.test.ts
+++ tests/aliases-variables.test.ts
@@ -1,8 +1,15 @@
 import postcss from 'postcss';
 import postcssRTLCSS from '../src';
 import { PluginOptions } from '../src/@types';
-import { readCSSFile, runTests } from './utils';
+import {
+  readCSSFile,
+  runTests,
+  createSnapshotFileName
+} from './utils';
 import { aliases } from './constants';
+import 'jest-specific-snapshot';
+
+const BASE_NAME = 'aliases-variables';
 
 const baseOptions: PluginOptions = { aliases };
 
@@ -19,35 +26,45 @@ runTests(baseOptions, (pluginOptions: PluginOptions): void => {
     it('aliases default', (): void => {
       const options: PluginOptions = { ...pluginOptions, aliases: {} };
       const output = postcss([postcssRTLCSS(options)]).process(input);
-      expect(output.css).toMatchSnapshot();
+      expect(output.css).toMatchSpecificSnapshot(
+        createSnapshotFileName(BASE_NAME,'aliases-default', pluginOptions.mode)
+      );
       expect(output.warnings()).toHaveLength(0);
     });
   
     it('aliases map', (): void => {
       const options: PluginOptions = { ...pluginOptions };
       const output = postcss([postcssRTLCSS(options)]).process(input);
-      expect(output.css).toMatchSnapshot();
+      expect(output.css).toMatchSpecificSnapshot(
+        createSnapshotFileName(BASE_NAME,'aliases-map', pluginOptions.mode)
+      );
       expect(output.warnings()).toHaveLength(0);
     });
   
     it('wrong aliases', (): void => {
       const options: PluginOptions = { ...pluginOptions, aliases: {base: true, parse: false} as unknown as Record<string, string> };
       const output = postcss([postcssRTLCSS(options)]).process(input);
-      expect(output.css).toMatchSnapshot();
+      expect(output.css).toMatchSpecificSnapshot(
+        createSnapshotFileName(BASE_NAME,'wrong-aliases', pluginOptions.mode)
+      );
       expect(output.warnings()).toHaveLength(0);
     });
 
     it('processEnv = true', (): void => {
       const options: PluginOptions = { ...pluginOptions };
       const output = postcss([postcssRTLCSS(options)]).process(input);
-      expect(output.css).toMatchSnapshot();
+      expect(output.css).toMatchSpecificSnapshot(
+        createSnapshotFileName(BASE_NAME,'process-env-true', pluginOptions.mode)
+      );
       expect(output.warnings()).toHaveLength(0);
     });
 
     it('processEnv = false', (): void => {
         const options: PluginOptions = { ...pluginOptions, processEnv: false };
         const output = postcss([postcssRTLCSS(options)]).process(input);
-        expect(output.css).toMatchSnapshot();
+        expect(output.css).toMatchSpecificSnapshot(
+          createSnapshotFileName(BASE_NAME,'process-env-false', pluginOptions.mode)
+        );
         expect(output.warnings()).toHaveLength(0);
     });
   
